1df4591dc8adca4c804f465cec2215cb2ee0fa5c
Kamanashis Roy <kamanashisroy+aroopcode@gmail.com>
2016-02-02 17:10:15 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Cloc Pie
+==========
+
+This software generates pie chart based on [cloc](https://github.com/AlDanial/cloc) output. The code defines which [langauge you are](http://miniim.blogspot.com/2016/02/line-of-code.html). To get to know yourself there are following steps,
+
+1. install [cloc](https://github.com/AlDanial/cloc).
+2. get the code statistics of each project.
+3. Create summary of all the statistics.
+```
+$(CLOC) --sum-reports --out summary.txt *.txt
+```
+4. Use clock pie to render pie chart.
+```
+pie.py summary.txt.lang
+```
+
+And that will generate a nice `pie.svg` file.
---
+++ pie.py
@@ -0,0 +1,43 @@
+"""
+Make a pie chart - see
+http://matplotlib.sf.net/matplotlib.pylab.html#-pie for the docstring.
+
+This example shows a basic pie chart with labels optional features,
+like autolabeling the percentage, offsetting a slice with "explode",
+adding a shadow, and changing the starting angle.
+
+"""
+from pylab import *
+import re
+
+# read cloc generated file
+lineno = 0
+# The slices will be ordered and plotted counter-clockwise.
+labels = []
+fracs = []
+#labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+#fracs = [15, 30, 45, 10]
+#explode=(0, 0.05, 0, 0)
+
+with open(sys.argv[1], 'r') as f:
+	for line in f:
+		lineno += 1
+		if lineno < 5 :
+			continue
+		if line.startswith('--'):
+			continue
+		if line.startswith('SUM'):
+			continue
+		m = re.search('^([a-zA-Z/+\s4]+)+\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)$', line);
+		labels.append(m.group(1))
+		fracs.append(m.group(5))
+f.closed
+
+# make a square figure and axes
+figure(1, figsize=(6,6))
+ax = axes([0.1, 0.1, 0.8, 0.8])
+pie(fracs, labels=labels, autopct='%1.1f%%', shadow=True)
+#title('Language pie', bbox={'facecolor':'0.8', 'pad':5})
+
+#show()
+savefig("pie.svg")
